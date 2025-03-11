c12f465e6855a8073cc80c6fc0ba993dd5c85bd7
Yanrs16 <yanrodrigues973@gmail.com>
2025-03-11 08:33:34 -0300

    footer.php

--- footer.php
+++ footer.php
@@ -1,15 +1,14 @@
 <?php
 // footer.php
 ?>
-<footer class="bg-dark text-white py-3">
-    <div class="container text-center">
+<!-- Rodapé -->
+<section class="rodape">
+    <p>&copy; 2025 Restaurante XP dos Sabores. Todos os direitos reservados.</p>
+    <div>
         <a href="index4.html" class="text-decoration-none text-warning">Sobre nós</a> |
         <a href="avaliacoes.html" class="text-decoration-none text-warning">Avaliações</a>
-        <p class="mt-2 mb-0">&copy; 2025 Restaurante XP dos Sabores. Todos os direitos reservados.</p>
     </div>
-</footer>
+</section>
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
